a9aea4405b1c5ae7ab5959a7d916cd691b20b8ea
knutekje <knutekjersem@gmail.com>
2025-01-27 08:19:01 +0100

    improving error handling

--- AuthService/DTOs/LoginDto.cs
+++ AuthService/DTOs/LoginDto.cs
@@ -4,11 +4,9 @@ namespace AuthService.DTOs;
 
 public class LoginDto
 {
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
+ 
+    public required string Email { get; set; }
 
-    [Required]
-    [StringLength(100, MinimumLength = 8)]
-    public string Password { get; set; }
+
+    public required string Password { get; set; }
 }
--- AuthService/Models/User.cs
+++ AuthService/Models/User.cs
@@ -1,14 +1,14 @@
 namespace AuthService.Models;
 
-
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string PasswordHash { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
     public string Role { get; set; } = Roles.Receptionist;
 }
+
 
 
 public static class Roles
